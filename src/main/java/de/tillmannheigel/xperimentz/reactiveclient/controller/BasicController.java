c1a38771f1f064fe8be7e78acdc8bce72342b82f
Tillmann Heigel <heigel@tillmannheigel.de>
2017-05-21 15:21:21 +0200

    @GetMapping('/events') with Flux

--- src/main/java/de/tillmannheigel/xperimentz/reactiveclient/controller/BasicController.java
+++ src/main/java/de/tillmannheigel/xperimentz/reactiveclient/controller/BasicController.java
@@ -1,6 +1,9 @@
 package de.tillmannheigel.xperimentz.reactiveclient.controller;
 
+import java.nio.file.Files;
+import java.time.Duration;
 import java.util.Date;
+import java.util.stream.Stream;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import de.tillmannheigel.xperimentz.reactiveclient.model.Event;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -19,6 +23,15 @@ public class BasicController {
     @GetMapping("/event/{id}")
     Mono<Event> eventById(@PathVariable long id){
         return Mono.just(new Event(id, new Date()));
+    }
+
+    @GetMapping("/events")
+    Flux<Event> events(){
+        Flux<Event> eventFlux = Flux.fromStream(
+                Stream.generate(() -> new Event(System.currentTimeMillis(), new Date()))
+        );
+        Flux<Long> heartbeatFlux = Flux.interval(Duration.ofSeconds(1));
+        return Flux.zip(eventFlux, heartbeatFlux).map(objects -> objects.getT1());
     }
 
 }
